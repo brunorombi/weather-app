41f22779fcfd789d94d952c0c530d9e46457a5ca
Bruno Rombi <brunorombi@yahoo.com>
2026-05-13 18:21:38 -0300

    feat: add new icon feature

--- src/dom.js
+++ src/dom.js
@@ -3,9 +3,70 @@ const btn = document.querySelector("button");
 const input = document.querySelector("input");
 const weatherContent = document.querySelector("#content");
 
-form.addEventListener("submit", () => {
-    if(form.validity.valueMissing) {
-        console.log("teste")
-    }
-})
+import { getData, Weather } from "./api";
 
+export default function initApp() {
+  const form = document.querySelector("form");
+  const input = document.querySelector("input");
+  const errorMessage = document.querySelector(".error");
+
+  form.addEventListener("submit", async (e) => {
+    e.preventDefault();
+
+    errorMessage.classList.remove("active");
+    errorMessage.textContent = "";
+    const searchValue = input.value.trim();
+
+    if (input.validity.valueMissing) {
+      displayError("You must enter a city name");
+      return;
+    }
+
+    if (!/[a-zA-Z]/.test(searchValue)) {
+      displayError("Your search should contain only letters");
+      return;
+    }
+
+    const weather = await getWeather(searchValue);
+
+    if (weather) {
+      displayWeather(weather);
+      console.log(weather)
+    } else {
+      displayError(`Was not possible to find ${searchValue}`);
+    }
+  });
+}
+
+async function getWeather(value) {
+  try {
+    const data = await getData(value);
+    const weather = new Weather(data);
+    return weather;
+  } catch (error) {
+    console.error(error);
+    displayError();
+  }
+}
+
+function displayError(error) {
+  const errorMessage = document.querySelector(".error");
+  errorMessage.textContent = "";
+  errorMessage.classList.add("active");
+  errorMessage.textContent = error;
+}
+
+async function displayWeather(weather) {
+  document.querySelector("#content").style.display = "flex";
+
+  const icon = await import(`./assets/${weather.icon}.svg`)
+  const img = document.querySelector(".weather-icon");
+
+  img.src = icon.default;
+
+  document.querySelector(".city").textContent = weather.city;
+  document.querySelector(".temp").textContent = weather.temp;
+  // document.querySelector(".description").textContent = weather.description;
+  document.querySelector(".time").textContent = weather.time;
+  document.querySelector(".date").textContent = weather.date;
+}
--- src/index.js
+++ src/index.js
@@ -1,65 +1,6 @@
 import "./styles.css";
-import { getData, Weather } from "./api";
+
+import initApp from "./dom";
+initApp()
+
 console.log("Hello!");
-
-async function main() {
-  const weather = await getData("saopaulo");
-  console.log(weather);
-}
-
-// main()
-const form = document.querySelector("form");
-const input = document.querySelector("input");
-const errorMessage = document.querySelector(".error");
-const content = document.querySelector("#content");
-
-form.addEventListener("submit", async (e) => {
-  e.preventDefault();
-
-  errorMessage.classList.remove("active");
-  errorMessage.textContent = "";
-  const searchValue = input.value.trim();
-
-  if (input.validity.valueMissing) {
-    displayError("You must enter a city name");
-    return;
-  }
-  
-  if (!/[a-zA-Z]/.test(searchValue)) {
-    displayError("Your search should contain only letters");
-    return;
-  }
-
-  const weather = await getWeather(searchValue);
-
-  if(weather) {
-    displayWeather(weather);
-  } else {
-    displayError(`Was not possible to find ${searchValue}`)
-  }
-});
-
-async function getWeather(value) {
-  try {
-    const data = await getData(value);
-    const weather = new Weather(data);
-    return weather;
-  } catch (error) {
-    console.error(error);
-    displayError();
-  }
-}
-
-function displayError(error) {
-  errorMessage.textContent = "";
-  errorMessage.classList.add("active")
-  errorMessage.textContent = error;
-}
-
-function displayWeather(weather) {
-  document.querySelector(".city").textContent = weather.city;
-  document.querySelector(".temp").textContent = weather.temp;
-  document.querySelector(".description").textContent = weather.description;
-  document.querySelector(".time").textContent = weather.time;
-  document.querySelector(".date").textContent = weather.date;
-}
